12715594cec6a4190bafe6712a99f0e064f833b3
Maximilian <scientificresearch309@gmail.com>
2023-04-28 16:47:49 +0200

    books items and initialState added!

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,7 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const books = [
+  {
+    id: 1,
+    idCode: "buildingMicroservices",
+    title: "Building Microservices",
+  },
+  {
+    id: 2,
+    idCode: "gatsbyEcommerce",
+    title: "Gatsby E-Commerce",
+  },
+  {
+    id: 3,
+    idCode: "rustWeb",
+    title: "Rust Web Programming",
+  },
+];
+
 const initialState = {
-  count: 0,
+  //   count: 0,
+  books,
+  cartItems: [],
 };
 
 export const cartSlice = createSlice({
