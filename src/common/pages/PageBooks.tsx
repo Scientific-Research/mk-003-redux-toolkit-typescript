c83531f90a0f1c303ce9e0c24b8079c40d422934
Maximilian <scientificresearch309@gmail.com>
2023-04-28 15:34:49 +0200

    dispatch was defined!

--- src/common/pages/PageBooks.tsx
+++ src/common/pages/PageBooks.tsx
@@ -1,7 +1,9 @@
 import { useDispatch } from "react-redux";
-import {add,subtract} from "../../features/cart/cartSlice"
+import { add, subtract } from "../../features/cart/cartSlice";
 
 export const PageBooks = () => {
+  const dispatch = useDispatch();
+
   return (
     <>
       <p>Welcome to the Books page.</p>
