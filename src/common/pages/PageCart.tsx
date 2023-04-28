99683d29141f56961942db310e1c66d6b1a9d6d1
Maximilian <scientificresearch309@gmail.com>
2023-04-28 17:55:10 +0200

    cartItem uisng map was defined!

--- src/common/pages/PageCart.tsx
+++ src/common/pages/PageCart.tsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+
 export const PageCart = () => {
+  const selectCart = (state: RootState) => state.cart;
+  const { cartItems } = useSelector(selectCart);
+
   return (
-    <>
-      <p>Welcome to the Cart page.</p>
-    </>
+    <div className="pageCart">
+      {/* <p>Welcome to the Cart page.</p> */}
+      <div className="cartItems">
+        {cartItems.map((cartItem) => {
+          return <div className="cartItem">nnn</div>;
+        })}
+      </div>
+    </div>
   );
 };
