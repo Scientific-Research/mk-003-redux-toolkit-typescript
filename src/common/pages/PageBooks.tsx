09088f0307956338c5da4897d960d81d81c3848f
Maximilian <scientificresearch309@gmail.com>
2023-04-28 15:55:40 +0200

    useSelector, RootState used to get the state from cartSlice

--- src/common/pages/PageBooks.tsx
+++ src/common/pages/PageBooks.tsx
@@ -1,8 +1,12 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { add, subtract } from "../../features/cart/cartSlice";
+import { RootState } from "../../app/store";
 
 export const PageBooks = () => {
   const dispatch = useDispatch();
+
+  const selectCart = (state: RootState) => state.cart;
+  const { count } = useSelector(selectCart);
 
   return (
     <div className="pageBooks">
